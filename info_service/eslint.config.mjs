4ee7967cee1aacd7c0895c474ff8bcfd72d2bf02
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2024-05-28 20:24:25 +0530

    removed useless deps

--- info_service/eslint.config.mjs
+++ info_service/eslint.config.mjs
@@ -1,16 +1,9 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import prettylint from 'eslint-plugin-prettier/recommended';
-import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config({
     files: ['**/*.ts'],
-    extends: [
-        eslint.configs.recommended,
-        ...tseslint.configs.recommended,
-        prettylint.configs,
-        prettierConfig,
-    ],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
     rules: {
         '@typescript-eslint/array-type': 'error',
         '@typescript-eslint/consistent-type-imports': 'error',
